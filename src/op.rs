f2faef36e05903b9292cd92c4613412d68386cf7
h <h>
2024-07-20 13:18:51 -0400

    bri ish

--- src/op.rs
+++ src/op.rs
@@ -1,4 +1,21 @@
-pub enum Register {}
+pub enum Pin {
+    P0,
+    P1,
+    X0,
+    X1,
+    X2,
+    X3,
+}
+
+pub enum Register {
+    Acc,
+    Dat,
+    Pin(Pin),
+}
+
+pub struct Label(String);
+
+pub struct Condition(bool);
 
 pub enum RegImm {
     Reg(Register),
@@ -16,4 +33,14 @@ pub enum Opcode {
     Not,
     Dgt(RegImm),
     Dst(RegImm, RegImm),
+    Teq(RegImm, RegImm),
+    Tgt(RegImm, RegImm),
+    Tlt(RegImm, RegImm),
+    Tcp(RegImm, RegImm),
+}
+
+pub struct LOC {
+    cond: Option<Condition>,
+    lab: Option<Label>,
+    op: Opcode,
 }
